b837e5c06b9067b7b60b8442a36ef12865b4466d
amiba-xqq <1310305204@pku.edu.cn>
2025-05-27 11:19:21 +0800

    Update 6.TCGA_BRCA_related_analysis.R

--- WGRS_related_analysis/6.TCGA_BRCA_related_analysis.R
+++ WGRS_related_analysis/6.TCGA_BRCA_related_analysis.R
@@ -4,12 +4,17 @@ library(TCGAbiolinks)
 library(maftools)
 library(TCGAmutations)
 library(ggpubr)
+
+####分群，获取ATM低表达和高表达肿瘤####
+####Cluster the data to obtain tumors with low and high expression of ATM####
 load("TCGA_BRCA_TPM.Rdata")
 gexp <- as.data.frame(t(tpm["ATM",]))
 gexp$Tumor_Sample_Barcode <- gsub("\\.", "-", rownames(gexp))
 ATM_High_ID <- gexp[gexp$ATM > quantile(gexp$ATM,0.75),]$Tumor_Sample_Barcode
 ATM_Low_ID <- gexp[gexp$ATM < quantile(gexp$ATM,0.25),]$Tumor_Sample_Barcode
-####读取ATM高表达肿瘤突变MAF文件####
+
+####获取ATM高表达肿瘤突变MAF文件####
+####Obtain the MAF file for tumors with high expression of ATM####
 query_SNV <- GDCquery(
   project = "TCGA-BRCA", 
   data.category = "Simple Nucleotide Variation",
@@ -25,7 +30,8 @@ maf_raw_high <- read.maf(mafs)
 maf_table_high <- as.data.frame(maf_raw_high@data)
 mafSummary_high <- mafSummary(maf_raw_high)
 
-####读取ATM低表达肿瘤突变MAF文件####
+####获取ATM低表达肿瘤突变MAF文件####
+####Obtain the MAF file for tumors with low expression of ATM####
 query_SNV <- GDCquery(
   project = "TCGA-BRCA", 
   data.category = "Simple Nucleotide Variation",
@@ -42,6 +48,7 @@ maf_table_low <- as.data.frame(maf_raw_low@data)
 mafSummary_low <- mafSummary(maf_raw_low)
 
 #计算突变率
+#Calculate mutant rate
 high_gene_mutation <- mafSummary_high[["gene.summary"]]
 low_gene_mutation <- mafSummary_low[["gene.summary"]]
 high_gene_mutation$freq_high <- high_gene_mutation$MutatedSamples/nrow(mafSummary_high[["variants.per.sample"]])*-100
@@ -51,22 +58,22 @@ gene_mutation <- full_join(high_gene_mutation[,c("Hugo_Symbol","freq_high")],
                            low_gene_mutation[,c("Hugo_Symbol","freq_low")],by="Hugo_Symbol")
 gene_mutation[is.na(gene_mutation)] <- 0
 
-####挑选ATMi诱导高突变gene####
-load("3.各组突变相关gene分析.Rdata")
+####挑选出ATMi加入后突变数目增加的gene####
+####Select genes where the number of mutations increases after the addition of ATM inhibitors####
+load("2.mutant_gene.Rdata")
 total.gene$log2FC_EtoIvsEto <- log2((total.gene$EtoI+1)/(total.gene$Eto+1))
 total.gene$log2FC_EtoAvsEto <- log2((total.gene$EtoA+1)/(total.gene$Eto+1))
 EtoI.up.gene <- total.gene[total.gene$log2FC_EtoIvsEto>=log2(4),]$SYMBOL
 EtoA.up.gene <- total.gene[total.gene$log2FC_EtoAvsEto>=log2(4),]$SYMBOL
 
 gene_mutation2 <- gene_mutation[gene_mutation$Hugo_Symbol %in% EtoA.up.gene,]
-#作箱型图
 plot_data <- data.frame(
   Gene = gene_mutation2$Hugo_Symbol,
   mutant_ratio = c(abs(gene_mutation2$freq_high), abs(gene_mutation2$freq_low)),
   Group = rep(c("ATM_High", "ATM_Low"), each = nrow(gene_mutation2))
 )
 
-plot_data$Group <- factor(plot_data$Group, levels = c("ATM_High", "ATM_Low")) #设置好箱型图x轴各组的分布顺序
+plot_data$Group <- factor(plot_data$Group, levels = c("ATM_High", "ATM_Low")) 
 my_comparisons <- list( c("ATM_High", "ATM_Low"))
 ggviolin(plot_data,
          x="Group",
@@ -76,7 +83,7 @@ ggviolin(plot_data,
          ylab= "Mutation Rate (%)",
          legend.title="",
          palette = c("#00B9E3","#F8766D"),
-         add = "mean_se",#增加散点
+         add = "mean_se",
          shape = 21,
          fill="Group",
          size=0.75)+
@@ -84,20 +91,21 @@ ggviolin(plot_data,
         axis.title.y = element_text(size = 16),
         legend.position = "none")+
   stat_compare_means(comparisons = my_comparisons,method = "wilcox.test")
-ggsave(filename="./results/ATM低表达肿瘤中ATMi诱导高发突变gene的突变率更高.pdf",
+ggsave(filename="./results/genes frequently mutated by ATMi have higher mutation rates in ATM-low-expression tumors.pdf",
        width = 3,height = 6)
 
-####挑选IRAK1i诱导高突变gene####
+####挑选出IRAK1i加入后突变数目增加的gene####
+####Select genes where the number of mutations increases after the addition of IRAK1 inhibitors####
 gene_mutation2 <- gene_mutation[gene_mutation$Hugo_Symbol %in% EtoI.up.gene &
                                   gene_mutation$Hugo_Symbol!="TTN",]
-#作箱型图
+
 plot_data <- data.frame(
   Gene = gene_mutation2$Hugo_Symbol,
   mutant_ratio = c(abs(gene_mutation2$freq_high), abs(gene_mutation2$freq_low)),
   Group = rep(c("ATM_High", "ATM_Low"), each = nrow(gene_mutation2))
 )
 
-plot_data$Group <- factor(plot_data$Group, levels = c("ATM_High", "ATM_Low")) #设置好箱型图x轴各组的分布顺序
+plot_data$Group <- factor(plot_data$Group, levels = c("ATM_High", "ATM_Low"))
 my_comparisons <- list( c("ATM_High", "ATM_Low"))
 ggviolin(plot_data,
          x="Group",
@@ -107,7 +115,7 @@ ggviolin(plot_data,
          ylab= "Mutation Rate (%)",
          legend.title="",
          palette = c("#00B9E3","#F8766D"),
-         add = "mean_se",#增加散点
+         add = "mean_se",
          shape = 21,
          fill="Group",
          size=0.75)+
@@ -115,5 +123,5 @@ ggviolin(plot_data,
         axis.title.y = element_text(size = 16),
         legend.position = "none")+
   stat_compare_means(comparisons = my_comparisons,method = "wilcox.test")
-ggsave(filename="./results/ATM低表达肿瘤中IRAK1i诱导高发突变gene的突变率更高.pdf",
+ggsave(filename="./results/genes frequently mutated by IRAK1i have higher mutation rates in ATM-low-expression tumors.pdf",
        width = 3,height = 6)
