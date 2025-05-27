24036fed4ef1cbc6b4eb53e79304a0e0dfe34755
amiba-xqq <1310305204@pku.edu.cn>
2025-05-27 10:50:54 +0800

    Update 1.vcf_clean.R

--- WGRS_related_analysis/1.vcf_clean.R
+++ WGRS_related_analysis/1.vcf_clean.R
@@ -2,9 +2,10 @@ rm(list = ls())
 library(tidyverse)
 ####读取注释后的vcf文件####
 ####Read the annotated VCF file####
+#ctrl means control group;Eto means only Etoposide treatment;EtoA means combination of Etoposide and ATM inhibitor;EtoI means combination of Etoposide and IRAK1 inhibitor.
 ctrl <- read.csv("./vcf_annotation/1WT.hg38_multianno.csv")
 ctrl$type <- paste0(ctrl$Chr,"_",ctrl$Start,"_",ctrl$End,"_",ctrl$Ref,"_",ctrl$Alt)
-#2Eto
+
 Eto <- read.csv("./vcf_annotation/1Eto.hg38_multianno.csv")
 Eto$type <- paste0(Eto$Chr,"_",Eto$Start,"_",Eto$End,"_",Eto$Ref,"_",Eto$Alt)
 Eto_diff <- Eto[(!(Eto$type %in% ctrl$type))&(Eto$Chr != "chrY")&(Eto$Chr != "chrY_KI270740v1_random"),]
@@ -12,7 +13,6 @@ Eto_diff <- Eto_diff[,-61]
 write.csv(Eto_diff,"./vcf_clean/1Eto_vcf_diff.csv")
 rm(Eto,Eto_diff)
 
-#3EtoA
 EtoA <- read.csv("./vcf_annotation/2EtoA.hg38_multianno.csv")
 EtoA$type <- paste0(EtoA$Chr,"_",EtoA$Start,"_",EtoA$End,"_",EtoA$Ref,"_",EtoA$Alt)
 EtoA_diff <- EtoA[(!(EtoA$type %in% ctrl$type))&(EtoA$Chr != "chrY")&(EtoA$Chr != "chrY_KI270740v1_random"),]
@@ -20,7 +20,6 @@ EtoA_diff <- EtoA_diff[,-61]
 write.csv(EtoA_diff,"./vcf_clean/2EtoA_vcf_diff.csv")
 rm(EtoA,EtoA_diff)
 
-#4CPTI
 EtoI <- read.csv("./vcf_annotation/3EtoI.hg38_multianno.csv")
 EtoI$type <- paste0(EtoI$Chr,"_",EtoI$Start,"_",EtoI$End,"_",EtoI$Ref,"_",EtoI$Alt)
 EtoI_diff <- EtoI[(!(EtoI$type %in% ctrl$type))&(EtoI$Chr != "chrY")&(EtoI$Chr != "chrY_KI270740v1_random"),]
